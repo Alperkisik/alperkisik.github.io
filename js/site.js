038e02194b51f9a603f1f6706ab8f3ab5bd75d47
Alperkisik <alperksk_07@hotmail.com>
2024-04-28 13:33:44 +0300

    commit

--- js/site.js
+++ js/site.js
@@ -1,39 +1,43 @@
-window.addEventListener('scroll', function () {
-    var header = document.querySelector('.header-top');
-    
-    header.classList.toggle('py-3', window.scrollY > 120);
-    header.classList.toggle('box-shadow-white', window.scrollY > 120);
+window.addEventListener("scroll", function () {
+  var header = document.querySelector(".header-top");
 
-    if(window.innerWidth >= 500){
-        header.classList.toggle('bg-transparent', window.scrollY <= 120);
-        header.classList.toggle('bg-dark', window.scrollY > 120);
-    }
+  header.classList.toggle("py-3", window.scrollY > 120);
+  header.classList.toggle("box-shadow-white", window.scrollY > 120);
+
+  if (window.innerWidth >= 500) {
+    header.classList.toggle("bg-transparent", window.scrollY <= 120);
+    header.classList.toggle("bg-dark", window.scrollY > 120);
+  }
 });
 
 window.onload = function () {
-    window.onscroll();
+  window.onscroll();
 };
 
 window.onscroll = function () {
-    scrollFunction();
+  scrollFunction();
 };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("goToTopBtn").style.display = "block";
-    } else {
-        document.getElementById("goToTopBtn").style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("goToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("goToTopBtn").style.display = "none";
+  }
 }
 
 function goToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 const pageContentNavigationClick = (targetId) => {
+  const element = document.getElementById(targetId);
 
-    const element = document.getElementById(targetId);
+  element.scrollIntoView({
+    behavior: "smooth",
+    block: "nearest",
+    inline: "start",
+  });
+};
 
-    element.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
-}
